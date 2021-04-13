fe58747c510eb83053b8e203521aef66cc9d815b
ImForeverYoung <adzhumadiev@mail.ru>
2021-04-13 20:15:58 +0600

    дз 15

--- hw-15/recursion.js
+++ hw-15/recursion.js
@@ -2,7 +2,7 @@ const reverseStringRecursive = (string, auxiliary = "") => {
     let reversedString = auxiliary;
     reversedString += string[string.length - 1];
     string = string.substring(0, string.length - 1);
-    if (string.length === 0) { // пока не дойдем до начала строки
+    if (string.length === 0) { // пока не дойдем до начала строки 
         return reversedString;
     } else {
         return reverseStringRecursive(string, reversedString);
